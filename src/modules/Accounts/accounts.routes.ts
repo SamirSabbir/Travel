0878c07d60db834097c53014c0af3fbd1b82aac4
Shahriar-Samir <shabusiness035@gmail.com>
2025-08-08 15:33:26 +0600

    update

--- src/modules/Accounts/accounts.routes.ts
+++ src/modules/Accounts/accounts.routes.ts
@@ -1,12 +1,20 @@
 // routes/account.route.ts
 import express from 'express';
 import { auth } from '../../middlewares/auth';
-import { handleCreateAccount, handleGetAccountById, handleGetAllAccounts } from './accounts.controller';
+import {
+  handleCreateAccount,
+  handleGetAccountById,
+  handleGetAllAccounts,
+} from './accounts.controller';
 
 const router = express.Router();
 
 router.post('/', auth('SuperAdmin', 'AccountAdmin'), handleCreateAccount);
-router.get('/', auth('SuperAdmin', 'AccountAdmin'), handleGetAllAccounts);
+router.get(
+  '/all-accounts',
+  auth('SuperAdmin', 'AccountAdmin'),
+  handleGetAllAccounts,
+);
 router.get('/:id', auth('SuperAdmin', 'AccountAdmin'), handleGetAccountById);
 
 export const accountRoutes = router;
