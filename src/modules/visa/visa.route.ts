c70d18b37534b1d2ae3d30fb64bb09607f6b7bdd
Shahriar-Samir <shabusiness035@gmail.com>
2025-09-20 15:18:50 +0600

    update

--- src/modules/visa/visa.route.ts
+++ src/modules/visa/visa.route.ts
@@ -12,3 +12,5 @@ visaRoutes.get('/', auth('SuperAdmin'), getAllVisasController);
 visaRoutes.get('/user', auth('Employee','SuperAdmin'), getVisaByAssignedToController);
 visaRoutes.patch('/:id', auth('Employee'), updateVisaByIdController);
 visaRoutes.patch('/update-customer-details/:id', auth('Employee'), updateVisaByIdController);
+visaRoutes.patch('/update-customer-details-usa/:id', auth('Employee'), updateVisaByIdController);
+visaRoutes.patch('/update-customer-details-schengen/:id', auth('Employee'), updateVisaByIdController);
