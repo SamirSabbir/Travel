176434a59d021ccc0248004d85ba0886e8764993
Shahriar-Samir <shabusiness035@gmail.com>
2025-11-26 15:57:48 +0600

    update

--- src/app.ts
+++ src/app.ts
@@ -12,7 +12,7 @@ const httpServer: HTTPServer = createServer(app);
 // Initialize Socket.IO
 const io = new SocketIOServer(httpServer, {
   cors: {
-    origin: 'http://localhost:5173',
+    origin: 'https://travel-trip-client.vercel.app',
     methods: ['GET', 'POST', 'PATCH', 'DELETE'],
   },
 });
